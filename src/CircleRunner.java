d555236b183feb196ec8e6fff4242e367af573b3
Edmond Song <songedmond777@gmail.com>
2022-09-30 11:29:28 -0400

    hi

--- src/CircleRunner.java
+++ src/CircleRunner.java
@@ -2,13 +2,15 @@ public class CircleRunner {
     public static void main(String[] args) {
         // A. create a Circle object with radius 5.0
         Circle circle = new Circle(5.0);
-        circle.printArea();
 
         // B. locate the method defined in the Circle class and write
         //    a line of code to call it on your object
+        circle.printArea();
 
         // C. BEFORE running this code, PREDICT what gets printed
+        // The area is 78.5
 
         // D. run the code and confirm your prediction! Were you correct?
+        //Yes
     }
 }
